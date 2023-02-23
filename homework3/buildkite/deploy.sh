6387c076f8004cf900af4c4ecd690e5d996061d0
cmuhao <sduxuhao@gmail.com>
2023-02-22 18:49:57 -0800

    update shell

--- homework3/buildkite/deploy.sh
+++ homework3/buildkite/deploy.sh
@@ -2,19 +2,13 @@
 
 set -euo pipefail
 
-if [ -z "${DOCKER_IMAGE:-}" ]; then
-  echo ":boom: \$DOCKER_IMAGE missing" 1>&2
-  exit 1
-fi
-
 manifest="$(mktemp)"
 
 echo '--- :kubernetes: Shipping'
 
 ls
 
-envsubst < ./homework2/kubernetes/ui/deployment.yaml > "${manifest}"
-kubectl apply -f "${manifest}"
+kubectl apply -f ./homework2/kubernetes/ui/deployment.yaml
 
 echo '--- :zzz: Waiting for deployment'
-kubectl wait --for condition=available --timeout=300s -f "${manifest}"
+kubectl wait --for condition=available --timeout=300s -f ./homework2/kubernetes/ui/deployment.yaml
