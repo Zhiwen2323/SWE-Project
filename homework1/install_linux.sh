89a7ec9e49dd29ba0a14c2592d88945c9caed0e5
cmuhao <sduxuhao@gmail.com>
2023-02-22 17:25:20 -0800

    update install shell

---
+++ homework1/install_linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check if package manager is installed
+if ! command -v apt-get > /dev/null; then
+  echo "Package manager is not installed. Please install it first."
+  exit 1
+fi
+
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y nodejs mysql-server python3-pip
+
+# Link python to python3
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
+# Start mysql service
+sudo service mysql start
+
+# Install React and ReactDOM as dependencies
+npm install react react-dom
+
+# Install Flask and OpenAI
+pip3 install aiohttp flask flask-cors flask-mysql openai flask-session
