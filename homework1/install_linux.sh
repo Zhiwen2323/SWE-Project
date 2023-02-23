caa4847522b09a736522918d3e7348a31e694106
cmuhao <sduxuhao@gmail.com>
2023-02-22 17:33:35 -0800

    remove sudo

--- homework1/install_linux.sh
+++ homework1/install_linux.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install required packages
-sudo apt-get update
-sudo apt-get install -y nodejs mysql-server python3-pip
+apt-get update
+apt-get install -y nodejs mysql-server python3-pip
 
 # Link python to python3
-sudo ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/python3 /usr/bin/python
 
 # Start mysql service
-sudo service mysql start
+service mysql start
 
 # Install React and ReactDOM as dependencies
 npm install react react-dom
