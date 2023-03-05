8bc01a31b64fecbd901279a5c86898c3436766dc
cmuhao <sduxuhao@gmail.com>
2023-03-04 16:28:16 -0800

    update hw3

--- homework3/infras/README.md
+++ homework3/infras/README.md
@@ -1,4 +1,26 @@
 # Infrastructure setups
 
-Simply run `./startup.sh` to startup all required infras including buildkite agent and
-rabbitmq cluster.
+## How to create kubernetes cluster in GCP
+1. Create the cluster in GCP console
+![cluster_view.png](images%2Fcluster_view.png)
+
+2. Choose Standard: You manage your cluster
+![create_cluster_view.png](images%2Fcreate_cluster_view.png)
+
+3. Configure: \
+Name: chatgpt \
+Zone: us-east1-b \
+![cluster_config_view.png](images%2Fcluster_config_view.png)
+
+4. Click CREATE
+
+## How to install required infras
+1. Connect to your cluster by clicking CONNECT:
+![connect_cluster.png](images%2Fconnect_cluster.png)
+2. Then click Run in cloud shell
+3. Type ENTER in terminal to execute the command and authorize.
+4. git clone repository and cd into it
+5. Simply run `./homework3/infras/startup.sh <BUILDKITE_AGENT_TOKEN>` with your buildkite agent token to startup all
+required infras including buildkite agent and rabbitmq cluster.
+
+
--- homework3/infras/startup.sh
+++ homework3/infras/startup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-KUBE_CLUSTER_NAME=$KUBE_CLUSTER_NAME
-KUBE_CLUSTER_ZONE=KUBE_CLUSTER_ZONE
-AGENT_TOKEN=$AGENT_TOKEN
+KUBE_CLUSTER_NAME=chatgpt
+KUBE_CLUSTER_ZONE=us-east1-b
+AGENT_TOKEN="$1"
+
+CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Get kubernetes config
 gcloud container clusters get-credentials $KUBE_CLUSTER_NAME --zone $KUBE_CLUSTER_ZONE
 
 # Create buildkite agent secret
+kubectl delete secret buildkite-agent --ignore-not-found
 kubectl create secret generic buildkite-agent --from-literal=token=$AGENT_TOKEN
 
 # create service account
-kubectl apply -f sa.yaml
+kubectl apply -f $CURRENT_DIR/sa.yaml
 
 # start buildkite-agent
-kubectl apply -f buildkite-agent.yaml
+kubectl apply -f $CURRENT_DIR/buildkite-agent.yaml
 
 
 # install rabbitmq kubernetes operator
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
 
 # create rabbitmq cluster
-kubectl apply -f logging_rabbit.yaml
+kubectl apply -f $CURRENT_DIR/logging_rabbit.yaml
